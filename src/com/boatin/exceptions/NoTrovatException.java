ba7111731ca6bd716dced4f51b5adaa6fd3c0722
Joanmi <Joanmi@DESKTOP-CI4B5PU>
2016-05-17 18:43:22 +0200

    Exceptions i eina per dates

---
+++ src/com/boatin/exceptions/NoTrovatException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.boatin.exceptions;
+
+/**
+ *
+ * @author Joanmi
+ */
+public class NoTrovatException extends Exception{
+    public NoTrovatException(){
+        
+    }
+    
+    public NoTrovatException(String text){
+        super(text);
+    }
+    
+}
--- src/com/boatinc/eines/Eina.java
+++ src/com/boatinc/eines/Eina.java
@@ -1,15 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.boatinc.eines;
+
+import com.boatin.exceptions.DataException;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
  * @author Joanmi
  */
 public class Eina {
-    
-    
+    public static Date creaDate(String fechaString) throws DataException{
+        int any,mes,dia;
+        Date fecha;
+        
+        if(fechaString.length()!=10){
+            throw new DataException("El format de la data no es correcte!");
+        }else{
+            dia=Integer.parseInt(fechaString.substring(0,2));
+            mes=Integer.parseInt(fechaString.substring(3,5));
+            any=Integer.parseInt(fechaString.substring(6,10));
+            
+            if(dia>31 || mes>12 || any<1500){
+                throw new DataException("numero incongruents");
+            }
+            
+            GregorianCalendar dataGregorian = new GregorianCalendar(any,mes-1,dia);
+            fecha = dataGregorian.getTime();
+            
+            return fecha;
+        }
+    }
 }
+    
+    
+
