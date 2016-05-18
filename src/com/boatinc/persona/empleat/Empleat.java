2e14f7f35336447e8efdf08bf256402196451d2b
Joanmi <Joanmi@DESKTOP-CI4B5PU>
2016-05-18 19:41:31 +0200

    hola que tal

--- src/com/boatinc/persona/empleat/Empleat.java
+++ src/com/boatinc/persona/empleat/Empleat.java
@@ -23,11 +23,11 @@ public class Empleat extends Persona {
     public Empleat(String nom, String cognom, Document document, String numeroDocument, String adreça, int telefon, String email, float sou, String dataContracte) throws DataException {
         super(nom, cognom, document, numeroDocument, adreça, telefon, email);
         this.sou = sou;
+        dataAlta = Eina.creaDate(dataContracte);
 
         if (dataAlta == null) {
             throw new DataException("DATA FAIL");
         }
-        dataAlta = Eina.creaDate(dataContracte);
     }
 
     public Date getDataAlta() {
