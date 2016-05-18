c1296204875197af53d279e2347e045b538f86ac
Joanmi <Joanmi@DESKTOP-CI4B5PU>
2016-05-18 19:45:47 +0200

    hola que tal

--- src/com/boatinc/persona/empleat/Reparador.java
+++ src/com/boatinc/persona/empleat/Reparador.java
@@ -39,7 +39,7 @@ public class Reparador extends Empleat{
     
     @Override
     public String toString() {
-        return "Reparador{" + "habilitats=" + habilitats + '}';
+        return "Reparador{" +super.toString()+ "habilitats=" + habilitats + '}';
     }
     
     
